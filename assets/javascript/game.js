1447753c565546ff7261f26b20e2fe4bd023dbba
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-07-03 13:27:32 -0400

    added the java script functions for the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var words= [
+    "yeet",
+    "cookie",
+    "pretty",
+    "blue",
+    "red",
+    "apple",
+    "dollar"
+];
+
+var word= words[Math.floor(math.random()*words.length)];
+
+var answerArray=[];
+for (var i=0; i< word.length;i++) {
+    answerArray[i]="_";
+}
+
+var remainingLetters= word.length;
+
+while (remainingLetters>0) {
+
+    alert(answerArray.join(""));
+
+    var guess= prompt("Guess a letter, or click cancel to stop playing.");
+    if(guess=== null){
+        break;
+    } else if (guess.length !==1) {
+        alert("please enter a single letter.");
+    } else {
+        for (var j = 0; j < word.length; j++) {
+            if (word[j]===guess){
+                answerArray[j]= guess;
+                remainingLetters--;
+            }
+        }
+    }
+    //Take input from the player
+    //Update answerArray and remainingLetters for every correct guess
+}
