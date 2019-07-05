3809fa1f814ad4a9571480f207d8c8445ebe4d5a
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-07-05 11:07:52 -0400

    edited the javascript text and added some additional functions to the end

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,35 +6,87 @@ var words= [
     "blue",
     "red",
     "apple",
-    "dollar"
+    "dollar",
+    "Baby",
+    "Turtle",
+    "Camaro"
 ];
 
-var word= words[Math.floor(math.random()*words.length)];
 
-var answerArray=[];
-for (var i=0; i< word.length;i++) {
-    answerArray[i]="_";
+var word = words[Math.floor(Math.random() * words.length)];
+
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
 }
 
-var remainingLetters= word.length;
+var remainingLetters = word.length;
 
-while (remainingLetters>0) {
 
-    alert(answerArray.join(""));
+while (remainingLetters > 0) {
 
-    var guess= prompt("Guess a letter, or click cancel to stop playing.");
-    if(guess=== null){
-        break;
-    } else if (guess.length !==1) {
-        alert("please enter a single letter.");
-    } else {
-        for (var j = 0; j < word.length; j++) {
-            if (word[j]===guess){
-                answerArray[j]= guess;
-                remainingLetters--;
-            }
-        }
-    }
-    alert(answerArray.join(""));
-    alert("You guessed it! The word was "+ word);
+var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+if (guess === null) {
+
+break;
+
+} else if (guess.length !== 1) {
+alert("Please enter a single letter.");
+
+} else {
+
+
+for (var j = 0; j < word.length; j++) {
+  if (word[j] === guess) {
+    answerArray[j] = guess; remainingLetters--;
+  }
 }
+}
+
+}
+
+
+alert(answerArray.join(" "));
+alert("Good job! The answer was " + word);
+
+
+
+var word = pickWord();
+var answerArray = setupAnswerArray(word);
+var remainingLetters = word.length;
+var pickWord = function () {
+
+};
+
+var setupAnswerArray = function (word) {
+
+};
+var showPlayerProgress = functin (answerArray)
+{
+
+};
+
+var getGuess = function () {
+
+};
+
+var updateGameState = function (guess, word, answerArray) {
+
+};
+
+var showAnswerAndCongratulatePlayer = function (answerArray) {
+
+};
+while (remainingLetters > 0) {
+showPlayerProgress(answerArray);
+var guess = getGuess();
+if (guess === null) {
+break;
+} else if (guess.length !== 1) {
+alert("Please enter a single letter.");
+} else {
+var correctGuesses = updateGameState(guess, word, answerArray);
+remainingLetters -= correctGuesses;
+}
+}
+
+showAnswerAndCongratulatePlayer(answerArray);
