bc27dfa12732207f8177046b54c6fec4d9ed310e
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-07-06 17:25:34 -0400

    revised javascript text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,95 +1,100 @@
+// VARIABLES
+// ==========================================================================
 
-var words= [
-    "yeet",
-    "cookie",
-    "pretty",
-    "blue",
-    "red",
-    "apple",
-    "dollar",
-    "baby",
-    "turtle",
-    "camaro",
-    "entrepreneur",
-    "restaurant",
-    "burger"
-];
+var carMakeandModel = ["Ford F150","Toyota Rav4","Chevrolet Silverado","Nissan Rogue","Honda CRV","Toyota Camry","Honda Civic","Chevrolet Equinox","Toyota Corolla","Ford Escape","Honda Accord","Ford Explorer","Nissan Altima","Toyota Tacoma", "Nissan Sentra", "GMC Sierra", "Jeep Grand Cherokee", "Jeep Cherokee", "Jeep Wrangler", "Toyota Highlander"];
 
+var directionsText1 = document.getElementById("directions-text1");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var incorrectKeysText = document.getElementById("incorrect-keys");
+var userWinsText = document.getElementById("user-wins-text");
 
-var word = words[Math.floor(Math.random() * words.length)];
+// Set the first iteration boolean to true;
+var firstIteration = true;
+// Set the reamining unfilled letters to 0;
+var remainingCounter = 0;
+// Create a String array and push each character into an array.
+var computerGuessArray = [];
+// var MOdifiedComputerGuessArray = [];
+var userGuessArray = [];
+// Declare more variables
+var userGuess;
+var computerGuess;
+var randomNUmber;
 
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+    userWinsText.textContent = "";
+    
+    // Determines which key was pressed.
+    userGuess = event.key;
+    userGuess = String(userGuess);
+    userGuess.toLocaleLowerCase();
+
+    if (firstIteration == true) {
+        randomNUmber = Math.floor(Math.random() * carMakeandModel.length);
+        computerGuess = carMakeandModel[randomNUmber];
+        
+        // Create a String array and push each character into an array.
+        computerGuessArray = [];
+        userGuessArray = [];
+        for (i = 0; i < computerGuess.length; i++){
+            computerGuessArray.push(computerGuess.slice(i, i+1))
+        }
+        
+        // Replace the characters of the string with _
+        for (i = 0; i < computerGuessArray.length; i++){
+            if (computerGuessArray[i] == " ")  {
+                computerGuessArray[i] = " - ";
+            }
+            else {
+                computerGuessArray[i] = " _ ";
+            }
+        }
+
+        // Convert the array back to string.
+        computerChoiceText.textContent = "Here's the word(Note: hyphens(-) represent spaces):    " + computerGuessArray.join(" ");
+        
+        // Rebuild the computer guessed array.
+        computerGuessArray = [];
+        for (i = 0; i < computerGuess.length; i++){
+            computerGuessArray.push(computerGuess.slice(i, i+1))
+        }
+        
+    }
+    if (firstIteration == false){
+        if (userGuess != " "){
+            for (i = 0; i < computerGuessArray.length; i++){
+                if (computerGuessArray[i].toLocaleLowerCase() == userGuess) {
+                    userGuessArray[i] = computerGuessArray[i];
+                }
+                else if( computerGuessArray[i] == userGuessArray[i]){
+                    userGuessArray[i] = computerGuessArray[i];
+                }
+                else if (computerGuessArray[i] == " ")  {
+                    userGuessArray[i] = " - ";
+                }
+                else {
+                    userGuessArray[i] = " _ ";
+                    remainingCounter++;
+                }
+            }
+    
+            if (remainingCounter === 0){
+                computerChoiceText.textContent = "Here's the word(Note: hyphens(-) represent spaces):    " + userGuessArray.join(" ");
+                computerChoiceText.style.color = "green";
+                userWinsText.textContent = "You Win! Good job!.Press any key to restart the game";
+                userWinsText.style.color = "blue";
+                firstIteration = true;
+            }
+            else {
+                computerChoiceText.textContent = "Here's the word(Note: hyphens(-) represent spaces):    " + userGuessArray.join(" ");
+                directionsText1.textContent = " ";
+            }
+            remainingCounter = 0;
+        }
+    }
+    else{
+        firstIteration = false;
+    }
 }
 
-var remainingLetters = word.length;
-
-
-while (remainingLetters > 0) {
-
-var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-if (guess === null) {
-
-break;
-
-} else if (guess.length !== 1) {
-alert("Please enter a single letter.");
-
-} else {
-
-
-for (var j = 0; j < word.length; j++) {
-  if (word[j] === guess) {
-    answerArray[j] = guess; remainingLetters--;
-  }
-}
-}
-
-}
-
-
-alert(answerArray.join(" "));
-alert("Good job! The answer was " + word);
-
-
-
-var word = pickWord();
-var answerArray = setupAnswerArray(word);
-var remainingLetters = word.length;
-var pickWord = function () {
-
-};
-
-var setupAnswerArray = function (word) {
-
-};
-var showPlayerProgress = functin (answerArray)
-{
-
-};
-
-var getGuess = function () {
-
-};
-
-var updateGameState = function (guess, word, answerArray) {
-
-};
-
-var showAnswerAndCongratulatePlayer = function (answerArray) {
-
-};
-while (remainingLetters > 0) {
-showPlayerProgress(answerArray);
-var guess = getGuess();
-if (guess === null) {
-break;
-} else if (guess.length !== 1) {
-alert("Please enter a single letter.");
-} else {
-var correctGuesses = updateGameState(guess, word, answerArray);
-remainingLetters -= correctGuesses;
-}
-}
-
-showAnswerAndCongratulatePlayer(answerArray);
